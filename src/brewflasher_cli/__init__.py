3d3139074156defa184657e5c4dd61093473eb93
Thorrak <thorrak@excelinspace.com>
2023-08-12 08:48:40 -0400

    Fix packaging

--- src/brewflasher_cli/__init__.py
+++ src/brewflasher_cli/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from . import brewflasher_cli
-
-def main():
-    brewflasher_cli.main()
-
-if __name__ == "__main__":
-    brewflasher_cli.main()
+# from . import brewflasher_cli
+#
+# def main():
+#     brewflasher_cli.main()
+#
+# if __name__ == "__main__":
+#     brewflasher_cli.main()
